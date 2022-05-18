3166e8c9e0713cb9cb61522cae588eddf145ccb0
sae-github <miyasae1126@gmail.com>
2022-05-18 20:43:23 +0900

    feat: formatter Settings

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    semi: "error",
+    quotes: ["error", "single"],
+  },
+};
